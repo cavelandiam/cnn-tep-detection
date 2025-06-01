4de11d7e48e37c017d103b634aac5c821ca448b6
cavelandiam <cavelandiam@gmail.com>
2025-05-31 23:07:21 -0500

    Modify trim volumes

--- scripts/pretrain_rsna.py
+++ scripts/pretrain_rsna.py
@@ -11,6 +11,10 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras import Input
 from tensorflow.keras.layers import Conv3D, MaxPooling3D, GlobalAveragePooling3D, Dense, Dropout
+import warnings
+
+# Suprimir advertencias específicas de pydicom para VR UI
+warnings.filterwarnings('ignore', category=UserWarning, message='Invalid value for VR UI')
 
 # Configuración de logging
 logging.basicConfig(
@@ -27,13 +31,13 @@ def pretrain_model():
     train_csv = pd.read_csv(RSNA_CSV_TRAIN_DIR)
     model = create_model()
     
-    batch_size = 4  # Ajusta según tu RAM
+    batch_size = 1  # Ajusta según tu RAM
     steps_per_epoch = len([p for p in Path(RSNA_DATASET_TRAIN_DIR).iterdir() if p.is_dir()]) // batch_size
     
     model.fit(
         rsna_data_generator(RSNA_DATASET_TRAIN_DIR, train_csv, batch_size=batch_size),
         steps_per_epoch=steps_per_epoch,
-        epochs=10  # Ajusta según tus necesidades
+        epochs=1  # Ajusta según tus necesidades
     )
     
     # Guardar el modelo en formato .keras
@@ -45,7 +49,7 @@ def pretrain_model():
 # Definir la arquitectura del modelo
 def create_model():    
     inputs = Input(shape=(TARGET_DEPTH, *IMAGE_SIZE, 1))
-    xx = Conv3D(32, (3, 3, 3), activation='relu')(inputs)
+    x = Conv3D(32, (3, 3, 3), activation='relu')(inputs)
     x = MaxPooling3D((2, 2, 2))(x)  # reduce a (128, 112, 112, 32)
     
     x = Conv3D(64, (3, 3, 3), activation='relu')(x)
@@ -67,7 +71,7 @@ def create_model():
     return model
 
 # Generador de datos para RSNA
-def rsna_data_generator(directory, train_csv, batch_size=4):
+def rsna_data_generator(directory, train_csv, batch_size):
     studies = [p for p in Path(directory).iterdir() if p.is_dir()]
     for i in range(0, len(studies), batch_size):
         batch_studies = studies[i:i + batch_size]
@@ -141,7 +145,7 @@ def load_dicom_image(dicom_path, target_size):
         if img.ndim == 3 and img.shape[-1] == 3:
             logging.warning(f"Archivo {dicom_path} ignorado (es un resumen diagnóstico).")
             return None
-        img = np.clip(img, -1000, 1000)
+        img = np.clip(img, -1000, 1000) #limita los valores de los píxeles a un rango de -1000 a 1000 Hounsfield Units (HU)
         img = (img + 1000) / 2000
         img = resize(img, target_size, anti_aliasing=True, preserve_range=True)
         return np.expand_dims(img, axis=-1)
@@ -160,5 +164,8 @@ def pad_or_trim_volume(volume, target_depth):
         pad_after = pad_size - pad_before
         return np.pad(volume, ((pad_before, pad_after), (0, 0), (0, 0), (0, 0)), mode='constant', constant_values=0)
     else:
-        start = (D - target_depth) // 2
-        return volume[start:start + target_depth, :, :, :]
+        # Recorte simétrico
+        trim_size = D - target_depth
+        trim_before = trim_size // 2
+        trim_after = trim_size - trim_before
+        return volume[trim_before:D - trim_after, :, :, :]
