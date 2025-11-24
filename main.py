22d95ac104d3ed848e91aa6bda3c5b4c000b2c7e
cavelandiam <cavelandiam@gmail.com>
2025-11-24 14:43:53 -0500

    train final model

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def test_pipeline():
     logger.info("INICIANDO PRUEBA DEL PIPELINE: CNN-TEP DETECTION")
     logger.info("PROCESANDO DATOS DE ENTRENAMIENTO RSNA")
 
-    #history, best_auc = s1_improved_3dcnn_tep.pretrain_model()
+    history, best_auc = s1_improved_3dcnn_tep.pretrain_model()
 
     # logger.info(f"🎉 PREENTRENAMIENTO RSNA COMPLETADO! MEJOR AUC: {best_auc:.4f}")
 
@@ -27,8 +27,8 @@ def test_pipeline():
 
     logger.info("GENERANDO INFERENCIA DE TEP EN PACIENTES HUCSR")
 
-    patient_dir = os.path.join(config.INFERENCE_NEW_PATIENTS_DIR, "999999999")    
-    s4_inference_tep.run_inference_tep(patient_dir=patient_dir)
+    #patient_dir = os.path.join(config.INFERENCE_NEW_PATIENTS_DIR, "999999999")    
+    #s4_inference_tep.run_inference_tep(patient_dir=patient_dir)
 
     logger.info("FINALIZA EJECUIÓN DEL PIPELINE: CNN-TEP DETECTION")
 
